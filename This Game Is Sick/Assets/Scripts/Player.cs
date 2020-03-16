1481f06ea60e8013a4afa67e9124cdf49c3ea1e0
Troy Rooney <troyrooney39@gmail.com>
2020-03-16 14:25:21 +1100

    Created Player script, created movement script

---
+++ This Game Is Sick/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    bool m_Infected;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+
+
+
+    //------------------------------------------------------------
+    //DO NOT REMOVE FUNCTION. REQUIRED FOR UNITY TO WORK. DO NOT TOUCH!!!!!!!!!
+    //------------------------------------------------------------
+    public void SetInfected(bool infected)
+    {
+        m_Infected = infected;
+    }
+}
