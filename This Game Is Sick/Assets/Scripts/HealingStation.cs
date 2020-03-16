3118b45f48e4ac858e412a5cdb04b1c186bd9774
Grant Roberts <gred.roberts@gmail.com>
2020-03-16 16:22:44 +1100

    Moved to LWRP

---
+++ This Game Is Sick/Assets/Scripts/HealingStation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingStation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+
+        }
+    }
+}
