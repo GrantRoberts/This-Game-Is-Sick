052443adb06950c906d2ccd28a95b10156cf8cd3
Grant Roberts <gred.roberts@gmail.com>
2020-03-16 14:43:57 +1100

    Added Objective script for interacting with objectives
    Also update my own scene, and edited infecter script

--- This Game Is Sick/Assets/Scripts/Infecter.cs
+++ This Game Is Sick/Assets/Scripts/Infecter.cs
@@ -13,8 +13,8 @@ public class Infecter : MonoBehaviour
         if (other.tag == "Player")
         {
             collision.gameObject.GetComponent<Player>().SetInfected(true);
-            other.GetComponent<Rigidbody>().AddForce(transform.forward * 100.0f);
-            GetComponent<Rigidbody>().AddForce(transform.forward * 100.0f);
+            other.GetComponent<Rigidbody>().AddForce(transform.forward * m_PushForce);
+            GetComponent<Rigidbody>().AddForce(transform.forward * m_PushForce);
         }
     }
 }
---
+++ This Game Is Sick/Assets/Scripts/Objective.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective : MonoBehaviour
+{
+    private bool m_PlayerInRadius = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_PlayerInRadius)
+        {
+            if (Input.GetKey(KeyCode.Space))
+                Debug.Log("Yes");
+        }
+        else
+          Debug.Log(m_PlayerInRadius);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        m_PlayerInRadius = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        m_PlayerInRadius = false;
+    }
+}
