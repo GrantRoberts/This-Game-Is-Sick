5cbab47ab23296a812063486b0ae7d0a6868aed6
Grant Roberts <gred.roberts@gmail.com>
2020-03-16 14:56:27 +1100

    Forgot curly brackets

--- This Game Is Sick/Assets/Scripts/Infecter.cs
+++ This Game Is Sick/Assets/Scripts/Infecter.cs
@@ -11,8 +11,10 @@ public class Infecter : MonoBehaviour
         GameObject other = collision.gameObject;
 
         if (other.tag == "Player")
-        collision.gameObject.GetComponent<Player>().SetInfected(true);
-        other.GetComponent<Rigidbody>().AddForce(transform.forward * m_PushForce);
-        GetComponent<Rigidbody>().AddForce(transform.forward * m_PushForce);
+        {
+            collision.gameObject.GetComponent<Player>().SetInfected(true);
+            other.GetComponent<Rigidbody>().AddForce(transform.forward * m_PushForce);
+            GetComponent<Rigidbody>().AddForce(transform.forward * m_PushForce);
+        }
     }
 }
