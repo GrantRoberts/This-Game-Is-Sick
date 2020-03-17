73c20ab84bb9cbd7bbaa2ab3ade781a413a38546
Grant Roberts <gred.roberts@gmail.com>
2020-03-17 16:56:32 +1100

    End screen will put up main menu

---
+++ This Game Is Sick/Assets/Scripts/EndScreen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XboxCtrlrInput;
+
+public class EndScreen : MonoBehaviour
+{
+    public float m_InputDelayTimer = 0.0f;
+
+    public GameObject m_MainMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_InputDelayTimer <= 0.0f)
+        {
+            if (AnyButtonPressed())
+            {
+                m_MainMenu.SetActive(true);
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            m_InputDelayTimer -= Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// Check all face buttons to see if one has been pressed by the player.
+    /// </summary>
+    /// <returns>If a face button has been pressed.</returns>
+    private bool AnyButtonPressed()
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            if (XCI.GetButtonDown((XboxButton)i))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
